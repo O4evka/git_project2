5e2adb5741e42029b28c17d3da01e30c27169d40
user <odnodn12@yandex.ru>
2023-05-01 14:29:29 +0500

    smotrim

--- program.py
+++ program.py
@@ -1,1 +1,13 @@
-print('bot')
+import telebot
+
+
+token = '6147473906:AAG2LA74-m3v6WNRAOIXZs4GYrdvPuCTeYg'
+bot = telebot.Telebot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️ ")
+
+
+bot.infinity_poling()
